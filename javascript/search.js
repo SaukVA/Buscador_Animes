ffd77521407bdcfa8d1c1091f7a207061e1de759
Saul Verdu <kowsva16@gmail.com>
2022-05-10 18:37:56 +0200

    Versión 1.5

--- javascript/search.js
+++ javascript/search.js
@@ -7,23 +7,26 @@ error.style.color = '#b75b5b';
 var animes =  new Array(); 
 
 class Anime {
+    /* Constructor de la clase anime */
     constructor(anime){
         this.title = anime.title;
         this.synopsis = anime.synopsis;
         this.image = anime.images.jpg.large_image_url;
     }
 
+    /* Funcion que nos devulve como resultado toda la infromación de un anime */
     toElement() {
         let anime = document.createElement("div");
         let text = document.createElement("div");
-        let img = document.createElement("img");
+        let img = document.createElement("div");
         let title = document.createElement("h2");
         let synopsis = document.createElement("p");
 
         anime.className = "anime";
 
         img.className ="anime__image"
-        img.src = this.image;
+        img.style.background = "url(" + this.image + ") center no-repeat";
+        img.style.backgroundSize = "cover";
 
         text.className ="anime__text"
         
@@ -36,6 +39,16 @@ class Anime {
         anime.appendChild(text);
 
         return anime;
+    }
+    
+    /* Función que nos devuelve solamente la imagen del anime */
+    toImage(){
+        let img = document.createElement("div");
+        img.className ="anime__image hvr-float-shadow"
+        img.style.background = "url(" + this.image + ") center no-repeat";
+        img.style.backgroundSize = "cover";
+
+        return img;
     }
 }
 
@@ -52,7 +65,9 @@ const search = () =>{
                         animes.push(new Anime(dates));
                     });                    
                     content.innerHTML = "";
-                    content.appendChild(animes[0].toElement());
+                    animes.forEach(anime => {
+                        content.appendChild(anime.toImage());
+                    });
                 }
                 else{
                     content.innerHTML = "";
